48ba849f94a98d87237089c9112855538276bc47
Dusan Maliarik <dusan.maliarik@gmail.com>
2016-04-06 15:25:25 -0700

    Upgrade

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject accent "0.1.9"
+(defproject accent "0.1.10"
   :description "WebGL utility belt"
   :url "http://github.com/skrat/accent"
   :author "skrat"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2311"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.40"]
+                 [org.clojure/core.async "0.2.374"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
